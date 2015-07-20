d704ee4cde1850789d033f603f9594e3a13220ef
Aaron Parecki <aaron@parecki.com>
2015-07-20 08:55:13 -0700

    add schema

---
+++ schema/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) DEFAULT NULL,
+  `instagram_user_id` varchar(255) DEFAULT NULL,
+  `instagram_access_token` varchar(255) DEFAULT NULL,
+  `instagram_response` text,
+  `micropub_endpoint` varchar(255) DEFAULT NULL,
+  `micropub_access_token` text,
+  `micropub_response` text,
+  `micropub_success` tinyint(4) DEFAULT '0',
+  `date_created` datetime DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `last_micropub_response` text,
+  `last_instagram_photo` varchar(255) DEFAULT NULL,
+  `last_photo_date` datetime DEFAULT NULL,
+  `token_endpoint` varchar(255) DEFAULT NULL,
+  `authorization_endpoint` varchar(255) DEFAULT NULL,
+  `send_category_as_array` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
