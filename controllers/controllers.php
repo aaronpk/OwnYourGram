47a49fb79268890cb4610021fd605a02e37141f0
Aaron Parecki <aaron@parecki.com>
2015-09-05 09:56:48 -0700

    rearrange controllers

--- controllers/controllers.php
+++ controllers/controllers.php
@@ -9,66 +9,27 @@ function require_login(&$app) {
   }
 }
 
-$app->get('/auth/instagram', function() use($app) {
-  if(require_login($app)) {
-    $html = render('instagram-auth', array(
-      'title' => 'Connect Instagram'
-    ));
-    $app->response()->body($html);
-  }
+$app->get('/', function($format='html') use($app) {
+  $res = $app->response();
+
+
+  ob_start();
+  render('index', array(
+    'title' => 'OwnYourGram',
+    'meta' => ''
+  ));
+  $html = ob_get_clean();
+  $res->body($html);
 });
 
-$app->get('/auth/instagram-start', function() use($app) {
-  if(require_login($app)) {
-    $app->redirect('https://api.instagram.com/oauth/authorize/?client_id='.Config::$instagramClientID.'&redirect_uri='.Config::instagramRedirectURI().'&response_type=code&scope=comments');
-  }
+
+$app->get('/creating-a-token-endpoint', function() use($app) {
+  $app->redirect('http://indiewebcamp.com/token-endpoint', 301);
 });
-
-$app->get('/auth/instagram-callback', function() use($app) {
-  if(require_login($app)) {
-
-    $params = $app->request()->params();
-
-    if(!array_key_exists('code', $params)) {
-      // Error authorizing
-      $app->redirect('/auth/instagram');
-    } else {
-      $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL, 'https://api.instagram.com/oauth/access_token');
-      curl_setopt($ch, CURLOPT_POST, true);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
-        'client_id' => Config::$instagramClientID,
-        'client_secret' => Config::$instagramClientSecret,
-        'redirect_uri' => Config::instagramRedirectURI(),
-        'grant_type' => 'authorization_code',
-        'code' => $params['code']
-      )));
-      $response = curl_exec($ch);
-      $token = json_decode($response);
-
-      if(property_exists($token, 'access_token')) {
-        // Remove the Instagram account info from a past user account if it already exists
-        ORM::for_table('users')->where('instagram_user_id', $token->user->id)->find_result_set()
-          ->set('instagram_user_id','')
-          ->set('instagram_access_token','')
-          ->save();
-
-        // Update the user record with the instagram access token
-        $user = ORM::for_table('users')->find_one($_SESSION['user_id']);
-        $user->instagram_access_token = $token->access_token;
-        $user->instagram_user_id = $token->user->id;
-        $user->instagram_response = $response;
-        $user->save();
-      } else {
-        $app->redirect('/auth/instagram');
-      }
-    }
-
-    $app->redirect('/instagram');
-  }
+$app->get('/creating-a-micropub-endpoint', function() use($app) {
+  $html = render('creating-a-micropub-endpoint', array('title' => 'Creating a Micropub Endpoint'));
+  $app->response()->body($html);
 });
-
 
 $app->get('/instagram', function() use($app) {
   if($user=require_login($app)) {
