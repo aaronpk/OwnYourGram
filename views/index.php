3e49b080efc3fbbe6254f03573886e99f7d0a85f
Aaron Parecki <aaron@parecki.com>
2016-12-26 08:57:36 -0800

    use flexbox for photo list

--- views/index.php
+++ views/index.php
@@ -17,32 +17,35 @@
     <i><strong><?= $this->total_photos ?></strong> grams owned by <?= $this->total_users ?> users on their own sites and counting!</i>
   </div>
 
-  <div class="row top-users">
+  <div class="top-users">
     <h3>Top users this week</h3>
+    
     <?php
     if(count($this->users) == 0):
-    ?>
-
-    <p>Nobody yet! The stats reset every Sunday.</p>
-
-    <?php
+      ?>
+      <p>No photos were imported in the last 7 days.</p>
+      <?php
     endif;
-
-    foreach($this->users as $user):
     ?>
-      <div class="col-xs-6 col-md-3">
-        <div class="thumbnail">
-          <a href="<?= $user->last_micropub_url ?>"><img src="<?= $user->last_instagram_img_url ?>"></a>
-          <div class="caption">
-            <p><a href="<?= $user->url ?>"><?= friendly_url($user->url) ?></a></p>
-            <p>
-              <?= $user->num ?> this week<br>
-              <?= $user->photo_count ?> total<br>
-            </p>
+
+    <div class="photo-list">
+      <?php
+      foreach($this->users as $user):
+      ?>
+        <div class="photo">
+          <div class="thumbnail">
+            <a href="<?= $user->last_micropub_url ?>"><img src="<?= $user->last_instagram_img_url ?>"></a>
+            <div class="caption">
+              <p><a href="<?= $user->url ?>"><?= friendly_url($user->url) ?></a></p>
+              <p>
+                <?= $user->num ?> this week<br>
+                <?= $user->photo_count ?> total<br>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    <?php
-    endforeach;
-    ?>
+      <?php
+      endforeach;
+      ?>
+    </div>
   </div>
