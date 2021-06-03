3edf2e52aca582fd76c8bc1bf40dc9c7160d0ebc
Aaron Parecki <aaron@parecki.com>
2021-06-03 09:41:39 -0700

    update extension to extract photo

--- extension/run.js
+++ extension/run.js
@@ -5,17 +5,17 @@ This script is injected into the active tab when the button is pressed. This has
 and finds the script tag containing the photo data and sends a message to the background script.
 */
 
-window.__additionalDataLoaded = function(path, data) {
-  console.log(path, data);
-  // Send the photo details to the background script
-  chrome.runtime.sendMessage({ type: "own_this_photo", path: path, data: data });
-}
-
+var found = false;
 for(var i in document.scripts) {
   var js = document.scripts[i].innerText;
-  if(js && js.match(/^window.__additionalDataLoaded/)) {
+  if(js && js.match(/^window._sharedData/)) {
+    // Send the photo details to the background script
     eval(js);
+	chrome.runtime.sendMessage({ type: "own_this_photo", path: window.location, data: window._sharedData.entry_data.PostPage[0] });
+    found = true;
   }
 }
 
-
+if(!found) {
+	console.log("Error finding photo data in page");
+}
